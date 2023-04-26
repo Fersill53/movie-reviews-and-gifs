832c11b88e2174528b5886c4bc4c3bccb57e29d4
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-04-25 23:47:26 -0500

    futile attempt to impress teammates with javascript skill(added apikeys)

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//omdb api and url
+const omdbKey = "e317d6f9";
+const omdbUrl = " http://www.omdbapi.com/?i=tt3896198&apikey=e317d6f9";
+
+//giphy api and url
+const gifKey = "JpAI2mazvDYpnE8lPMeIKCL07UmSmkHa";
+const gifUrl = "api.giphy.com/v1/gifs/searh";
+
+//id's to be used
+//input: search, go: goBtn
+const movBtn = document.getElementById('goBtn')
