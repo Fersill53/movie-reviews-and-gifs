c3e9519ea83a37508c8199d020bb11e41cee8b77
Volcomix <m.santo13@hotmail.com>
2023-04-27 17:40:02 -0600

    Added gify fetch

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
 //omdb api and url
 const omdbKey = "e317d6f9";
-const movieTitle = "Shrek";
+let movieTitle = "Shrek";
 const omdbUrl = " http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + movieTitle;
 console.log(omdbUrl);
 
 //giphy api and url
+let gifImage = "Shrek";
 const gifKey = "JpAI2mazvDYpnE8lPMeIKCL07UmSmkHa";
-const gifUrl = "api.giphy.com/v1/gifs/searh";
+const gifUrl = "api.giphy.com/v1/gifs/searh" + gifKey;
+
 
 //id's to be used
 //input: search, go: goBtn
@@ -16,7 +18,7 @@ movBtn.addEventListener("click", function(event) {
     event.preventDefault();
     fetch (omdbUrl)
     .then(function(response){
-        return response.json();
+       return response.json();
     })
     .then(function(data) {
         console.log(data);
@@ -24,7 +26,16 @@ movBtn.addEventListener("click", function(event) {
     .catch(function (error){
         console.log(error);
     });
-
 });
 
+fetch (gifUrl)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function (error){
+    console.log(error);
+});
 
