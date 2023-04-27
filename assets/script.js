72dbf939ffab02be712adc46e2a533af5b118bb8
Volcomix <m.santo13@hotmail.com>
2023-04-26 19:58:45 -0600

    Added fetch and event listener

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 //omdb api and url
 const omdbKey = "e317d6f9";
-const omdbUrl = " http://www.omdbapi.com/?i=tt3896198&apikey=e317d6f9";
+const movieTitle = "Shrek";
+const omdbUrl = " http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + movieTitle;
+console.log(omdbUrl);
 
 //giphy api and url
 const gifKey = "JpAI2mazvDYpnE8lPMeIKCL07UmSmkHa";
@@ -9,3 +11,20 @@ const gifUrl = "api.giphy.com/v1/gifs/searh";
 //id's to be used
 //input: search, go: goBtn
 const movBtn = document.getElementById('goBtn')
+
+movBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    fetch (omdbUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function (error){
+        console.log(error);
+    });
+
+});
+
+
