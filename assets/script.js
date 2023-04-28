67a27735b4598c5a7d1b14078cb0b9de9d62b925
Volcomix <m.santo13@hotmail.com>
2023-04-27 19:59:56 -0600

    debugged GIPHY to display GIF

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 //omdb api and url
 const omdbKey = "e317d6f9";
-let movieTitle = "Shrek";
+let movieTitle = "";
 const omdbUrl = " http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + movieTitle;
 console.log(omdbUrl);
 
 //giphy api and url
-let gifImage = "Shrek";
+let gifImage = "";
 const gifUrl = "https://api.giphy.com/v1/gifs/search?api_key=Lelhn00NJhLelwpBhUe1XaTBsoJcTzvD&q=" + gifImage + "&limit=25&offset=0&rating=g&lang=en"
 
 
@@ -16,21 +16,29 @@ const movBtn = document.getElementById('goBtn')
 
 movBtn.addEventListener("click", function(event) {
      event.preventDefault();
-Promise.all([
-	fetch(omdbUrl),
-	fetch(gifUrl)
-]).then(function (responses) {
-	// Get a JSON object from each of the responses
-	return Promise.all(responses.map(function (response) {
-		return response.json();
-	}));
-}).then(function (data) {
-	// Log the data to the console
-	// You would do something with both sets of data here
-	console.log(data);
-}).catch(function (error) {
-	// if there's an error, log it
-	console.log(error);
-});
+    Promise.all([
+        fetch(omdbUrl),
+        fetch(gifUrl)
+    ]).then(function (responses) {
+        // Get a JSON object from each of the responses
+        return Promise.all(responses.map(function (response) {
+            return response.json();
+        }));
+    }).then(function (data) {
+        // Log the data to the console
+        // You would do something with both sets of data here
+        console.log(data);
+        var image1 = document.querySelector(".movieGif");
+        var img = document.createElement("img");
+        var image = data[1].data[0].images.downsized.url; 
+        image1.appendChild(img);
+        img.setAttribute("src", image);
+
+        
+
+    }).catch(function (error) {
+        // if there's an error, log it
+        console.log(error);
+    });
 
 });
